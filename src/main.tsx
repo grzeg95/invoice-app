bd1e79974f28e9a2d61f540d97c700338ddafa6a
Grzegorz Eleryk <grzegorz.eleryk@gmail.com>
2025-09-14 16:01:37 +0200

    feat: add breakpoints provider

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,16 @@ import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import './index.scss'
 import {App} from './App.tsx'
+import {BreakpointsProvider} from './context/Breakpoints/BreakpointsProvider.tsx';
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App/>
+      <BreakpointsProvider>
+        <App/>
+      </BreakpointsProvider>
     </QueryClientProvider>
   </StrictMode>
 );
