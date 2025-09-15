d211b11974fff1ec678e749a36650db5d88200cd
Grzegorz Eleryk <grzegorz.eleryk@gmail.com>
2025-09-15 13:33:59 +0200

    feat: add Nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import styles from './App.module.scss';
+import {Nav} from './components/Nav/Nav';
 
 export function App() {
 
   return (
     <main className={styles['main-container']}>
+      <Nav/>
       <div className={styles['content-container']}></div>
     </main>
   )
