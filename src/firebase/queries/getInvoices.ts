b701d817ceed6cce711cf0ed6f93ed2ceea9bec6
Grzegorz Eleryk <grzegorz.eleryk@gmail.com>
2025-09-18 09:56:39 +0200

    feat: add getInvoice firestore query

---
+++ src/firebase/queries/getInvoices.ts
@@ -0,0 +1,57 @@
+import {getDocs, limit, orderBy, query, QueryDocumentSnapshot, startAfter, where} from 'firebase/firestore';
+import {firestore} from '../firebase';
+import {getUserRef} from '../../models/firestore/user';
+import {getUserInvoiceCollectionRef, type UserInvoice} from '../../models/firestore/user-invoice';
+import type {InvoiceState} from '../../models/InvoiceState';
+
+export async function getInvoices(
+  pageParam: QueryDocumentSnapshot<UserInvoice> | null,
+  selectedOptions: InvoiceState[],
+  userUid: string,
+  limitOfItems: number
+) {
+
+  if (selectedOptions.length === 0) {
+    return {
+      invoices: [],
+      lastDocSnap: null
+    };
+  }
+
+  const userRef = getUserRef(firestore, userUid);
+  const userInvoiceCollectionRef = getUserInvoiceCollectionRef(userRef);
+
+  let q;
+
+  if (pageParam) {
+    q = query(
+      userInvoiceCollectionRef,
+      where('state', 'in', selectedOptions),
+      orderBy('createdAt', 'desc'),
+      orderBy('paymentDue', 'desc'),
+      limit(limitOfItems + 1),
+      startAfter(pageParam)
+    );
+  } else {
+    q = query(
+      userInvoiceCollectionRef,
+      where('state', 'in', selectedOptions),
+      orderBy('createdAt', 'desc'),
+      orderBy('paymentDue', 'desc'),
+      limit(limitOfItems + 1)
+    );
+  }
+
+  const invoicesQuerySnap = await getDocs(q);
+  let invoices = invoicesQuerySnap.docs;
+  let lastDocSnap;
+
+  if (invoices.length > limitOfItems) {
+    invoices = invoicesQuerySnap.docs.slice(0, -1);
+    lastDocSnap = invoicesQuerySnap.docs[limitOfItems - 1]
+  } else {
+    lastDocSnap = null;
+  }
+
+  return {invoices, lastDocSnap};
+}
