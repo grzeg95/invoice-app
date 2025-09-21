69c1e4bc7d797e84c99e3abcd71c9ad5b0d705fe
Grzegorz Eleryk <grzegorz.eleryk@gmail.com>
2025-09-21 09:36:38 +0200

    feat: install @tanstack/react-query-devtools

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+import {ReactQueryDevtools} from '@tanstack/react-query-devtools';
 import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import './index.scss'
@@ -19,6 +20,7 @@ createRoot(document.getElementById('root')!).render(
           </ThemeProvider>
         </AuthProvider>
       </BreakpointsProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </StrictMode>
 );
