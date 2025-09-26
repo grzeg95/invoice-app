35998f55e8890a68316adfa1254b26ebf9ab4df1
Grzegorz Eleryk <grzegorz.eleryk@gmail.com>
2025-09-26 11:29:01 +0200

    feat: handle dialog scroll visibility

--- src/components/ui/Dialog/Dialog.tsx
+++ src/components/ui/Dialog/Dialog.tsx
@@ -1,25 +1,52 @@
-import {type ReactNode, useEffect, useRef} from 'react';
+import {type ReactNode, type RefObject, useEffect, useImperativeHandle, useRef} from 'react';
 import {createPortal} from 'react-dom';
 import {useFocusTrap} from '../../../hooks/useFocusTrap';
+import {mutationObserverScrollVisibility} from '../../../utils/mutationObserverScrollVisibility';
+import {updateScrollVisibility} from '../../../utils/updateScrollVisibility';
 import styles from './Dialog.module.scss';
+
+let dialogIdCounter = 0;
+const dialogStack: number[] = [];
+
+export type DialogRef = {
+  close: () => void;
+};
 
 export type DialogProps = {
   children: ReactNode;
   onClose?: () => void;
-  isOpen: boolean
+  isOpen: boolean;
+  ref?: RefObject<DialogRef | null> | null;
 }
 
-export function Dialog({children, onClose, isOpen}: DialogProps) {
+export function Dialog({children, onClose, isOpen, ref}: DialogProps) {
 
+  const dialogId = useRef(-1);
   const dialogRef = useRef<HTMLDivElement>(null);
 
   const trapRef = useFocusTrap({
     active: isOpen
   });
 
+  useImperativeHandle(ref, () => {
+    return {
+      close() {
+        onClose?.();
+      }
+    };
+  }, [onClose]);
+
+  function isFirstDialog() {
+    return dialogStack.indexOf(dialogId.current) === 0;
+  }
+
   useEffect(() => {
 
     if (isOpen) {
+
+      dialogId.current = ++dialogIdCounter;
+      dialogStack.push(dialogIdCounter);
+
       const handleKeyDown = (event: KeyboardEvent) => {
         if (event.key === 'Escape') {
           onClose?.();
@@ -28,7 +55,34 @@ export function Dialog({children, onClose, isOpen}: DialogProps) {
 
       window.addEventListener('keydown', handleKeyDown);
 
-      return () => window.removeEventListener('keydown', handleKeyDown);
+      let observer: MutationObserver;
+
+      if (isFirstDialog()) {
+        updateScrollVisibility();
+        window.addEventListener('resize', updateScrollVisibility);
+        observer = new MutationObserver(mutationObserverScrollVisibility);
+        observer.observe(document.body, {childList: true, subtree: true});
+      }
+
+      return () => {
+
+        if (isFirstDialog()) {
+          window.removeEventListener('resize', updateScrollVisibility);
+          document.body.style.paddingRight = '0px';
+          document.body.style.overflow = 'auto';
+          document.body.style.overflow = 'auto';
+          observer?.disconnect();
+        }
+
+        window.removeEventListener('keydown', handleKeyDown);
+
+        const index = dialogStack.indexOf(dialogIdCounter);
+        dialogStack.splice(index, 1);
+
+        if (dialogStack.length === 0) {
+          dialogIdCounter = 0;
+        }
+      };
     }
   }, [isOpen, onClose]);
 
